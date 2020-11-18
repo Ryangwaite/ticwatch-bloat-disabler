7e17ec8e19b73995e2e065e7b80184a4b1beb5e7
Ryan Garthwaite <ryangwaite@gmail.com>
2020-11-18 20:10:56 +1000

    Implemented state machine in ConnectionManager

--- web-app/src/ConnectionManager.js
+++ web-app/src/ConnectionManager.js
@@ -1,10 +1,12 @@
 import Adb from 'webadb';
 
 /**
- * All possible states for the connection to the watch to be in
+ * All possible states for the ConnectionManager
  */
-const ConnectionState = Object.freeze({
-    CONNECTED:      0,
+const ConnectionManagerState = Object.freeze({
+    DISCONNECTED:       0,
+    USB_CONNECTED:      1,
+    ADB_CONNECTED:      2,
 });
 
 export default class ConnectionManager {
@@ -12,13 +14,17 @@ export default class ConnectionManager {
     constructor() {
         this._webUsbConnection = null;
         this._adbConnection = null;
-        this._state = null;
+        this._state = ConnectionManagerState.DISCONNECTED;
 
         // Bind methods to this class so we can call them correctly from eventhandlers
         this.initializeUsbConnection = this.initializeUsbConnection.bind(this);
         this.connectAdb = this.connectAdb.bind(this);
         this.closeConnection = this.closeConnection.bind(this);
     };
+
+    get state() {
+        return this._state;
+    }
 
     /**
      * Prompt user with chrome dialog to select the USB device.
@@ -27,10 +33,16 @@ export default class ConnectionManager {
      * Then connects to it at the USB level.
      */
     async initializeUsbConnection() {
+
+        if (this._state !== ConnectionManagerState.DISCONNECTED) {
+            console.warn("A USB connection has already been established");
+            return;
+        }
         
         try {
             // Prompt user to select adb device in chrome dialogue
             this._webUsbConnection = await Adb.open("WebUSB"); // "WebUSB" is the only allowed transport here
+            this._state = ConnectionManagerState.USB_CONNECTED;
             console.info("Successfully created USB Connection");
         } catch (error) {
             console.warn(`No ADB device selected. Error: ${error}`);
@@ -42,21 +54,35 @@ export default class ConnectionManager {
      * Prompts the user on the watch to allow this browser to create an ADB connection.
      */
     async connectAdb(watchPromptCallback = (productName) => {
-        console.warn(`Accept the ADB connection on your ${productName} watch.`);
-    }) {    
+        console.warn(`Accept the ADB connection on your ${productName} watch`);
+    }) {
+
+        // Exit with an appropriate warning if not in correct state
+        switch (this._state) {
+            case ConnectionManagerState.ADB_CONNECTED:
+                console.warn("An ADB connection has already been established");
+                return;
+            case ConnectionManagerState.DISCONNECTED:
+                console.warn("A USB connection must be established first");
+                return;
+            default:
+                break;
+        }
         
-        if (this._webUsbConnection && this._webUsbConnection.isAdb()) {
-            try {
-                // Connect to ADB with banner = "host::". IDK what this is but all the examples use this
-                this._adbConnection = await this._webUsbConnection.connectAdb("host::", () => {
-                    watchPromptCallback(this._webUsbConnection.device.productName);
-                });
-                console.info(`Successfully established ADB connection to ${this._webUsbConnection.device.productName}`);
-            } catch (error) {
-                console.error(`Failed to create an ADB connection: Error: ${error}`);    
-            }
-        } else {
-            console.error("Tried to create an ADB connection without a USB connection");
+        if (!this._webUsbConnection.isAdb()) {
+            console.error("Cannot initialize an ADB connection. The connected device is not ADB capable");
+        }
+
+        try {
+            // Connect to ADB with banner = "host::". IDK what this is but all the examples use this
+            this._adbConnection = await this._webUsbConnection.connectAdb("host::", () => {
+                watchPromptCallback(this._webUsbConnection.device.productName);
+            });
+            this._state = ConnectionManagerState.ADB_CONNECTED;
+            console.info(`Successfully established ADB connection to ${this._webUsbConnection.device.productName}`);
+        } catch (error) {
+            this._state = ConnectionManagerState.USB_CONNECTED;
+            console.error(`Failed to create an ADB connection: Error: ${error}`);    
         }
     };
 
@@ -64,11 +90,16 @@ export default class ConnectionManager {
      * Close the ADB and USB connection
      */
     async closeConnection() {
+
+        if (this._state === ConnectionManagerState.DISCONNECTED) {
+            console.warn("Tried to disconnect a device that wasn't connected");
+            return;
+        }
+
         if (this._webUsbConnection) {
             this._webUsbConnection.close();
+            this._state = ConnectionManagerState.DISCONNECTED;
             console.info("Successfully terminated the USB Connection");
-        } else {
-            console.warn("Tried to disconnect a device that wasn't connected");
         }
     };
 };

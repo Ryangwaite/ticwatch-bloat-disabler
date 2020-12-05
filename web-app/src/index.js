5ae0cedb2c3be63079e8fda2c7846f2e9761d1ef
Ryan Garthwaite <ryangwaite@gmail.com>
2020-12-05 22:31:39 +1000

    Added disconnect button and wired it up to disconnect the connection to the watch. When the connect button callback finishes it transitions into the disconnect button and vice-versa.

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -2,6 +2,11 @@ import './style.less'; // Js will dynamically add the CSS transpilation in the <
 import ConnectionManager from './ConnectionManager';
 import cross from './images/cross.svg';
 
+const ConnectionButtonMode = Object.freeze({
+    CONNECT:         0,
+    DISCONNECT:      1,
+});
+
 // Manages the connection state and allows for communication with the watch
 const connectionManager = new ConnectionManager;
 
@@ -19,32 +24,86 @@ function getTablePackageElements() {
     return tableDataElements.map(x => ({packageName: x.textContent.trim(), tableDataElement: x}));
 }
 
+/**
+ * Connects to the watch first over USB which causes the browser to
+ * prompt the user for a USB device. Once accepted, displays a modal
+ * prompting the user to accept the ADB connection on the watch. Next,
+ * fetches the watches stats and displays them in the stats section.
+ * Changes the connection button to "disconnect" mode
+ */
+async function connect() {
+    await connectionManager.initializeUsbConnection();
+
+    // TODO: Add a modal while waiting for adb connection to be accepted
+
+    await connectionManager.connectAdb();
+    let deviceStats = await connectionManager.getDeviceStats()
+            .then(stats => {
+                console.log("Got device stats from watch: ", stats);
+                return stats;
+            }).catch(err => {
+                // TODO: display this in the stats container
+                console.error("Failed to get device stats from watch. Error: ", err);
+            });
+
+    // Populate the device stats
+    document.getElementById("model_status").textContent = deviceStats.model;
+    document.getElementById("serialno_status").textContent = deviceStats.serialNo;
+    document.getElementById("android_status").textContent = deviceStats.androidVersion;
+    document.getElementById("battery_status").textContent = `${deviceStats.batteryPercent} %`;
+
+    // Hide the text stats container message and show the retrieved stats
+    document.querySelector("#watch-stats-container > .watch-stats-container__text").classList.add("watch-stats-container__text--hidden");
+    document.querySelector("#watch-stats-container > .watch-stats-container__grid").classList.remove("watch-stats-container__grid--hidden");
+
+    changeConnectionButtonTo(ConnectionButtonMode.DISCONNECT);
+}
+
+/**
+ * Closes the ADB connection to the watch. Clears the stats.
+ * Changes the connection button to "connect" mode.
+ */
+async function disconnect() {
+    await connectionManager.closeConnection();
+
+    // Clear the stats and show the no device connected message
+    document.querySelector("#watch-stats-container > .watch-stats-container__text").classList.remove("watch-stats-container__text--hidden");
+    document.querySelector("#watch-stats-container > .watch-stats-container__grid").classList.add("watch-stats-container__grid--hidden");
+
+    changeConnectionButtonTo(ConnectionButtonMode.CONNECT);
+}
+
+/**
+ * Changes the connection button to the state indicated by
+ * newButtonState.
+ * 
+ * Binds the click listener for the indicated state.
+ * 
+ * @param {number} newButtonState state to set the connection button to 
+ */
+function changeConnectionButtonTo(newButtonState) {
+    const connectionBtn = document.getElementById("connection-btn");
+
+    // Clear any current event listeners
+    connectionBtn.removeEventListener("click", connect);
+    connectionBtn.removeEventListener("click", disconnect);
+
+    switch (newButtonState) {
+        case ConnectionButtonMode.CONNECT:
+            connectionBtn.textContent = "Connect"; // Set button label
+            connectionBtn.addEventListener("click", connect);
+            break;
+        case ConnectionButtonMode.DISCONNECT:
+            connectionBtn.textContent = "Disconnect"; // Set button label
+            connectionBtn.addEventListener("click", disconnect);
+            break;
+        default:
+            throw `Invalid button state '${newButtonState}' for the connection button`;
+    }
+}
+
 // Bind click listeners
-document.getElementById("connection-btn")
-    .addEventListener("click", async () => {
-        await connectionManager.initializeUsbConnection();
-
-        // TODO: Add a toast while waiting for adb connection to be accepted
-
-        await connectionManager.connectAdb();
-        let deviceStats = await connectionManager.getDeviceStats()
-                .then(stats => {
-                    console.log("Got device stats from watch: ", stats);
-                    return stats;
-                }).catch(err => {
-                    console.error("Failed to get device stats from watch. Error: ", err);
-                });
-
-        // Populate the device stats
-        document.getElementById("model_status").textContent = deviceStats.model;
-        document.getElementById("serialno_status").textContent = deviceStats.serialNo;
-        document.getElementById("android_status").textContent = deviceStats.androidVersion;
-        document.getElementById("battery_status").textContent = `${deviceStats.batteryPercent} %`;
-
-        // Hide the text stats container message and show the retrieved stats
-        document.querySelector("#watch-stats-container > .watch-stats-container__text").classList.add("watch-stats-container__text--hidden");
-        document.querySelector("#watch-stats-container > .watch-stats-container__grid").classList.remove("watch-stats-container__grid--hidden");
-    });
+changeConnectionButtonTo(ConnectionButtonMode.CONNECT);
 
 document.getElementById("run-btn")
     .addEventListener("click", async () => {
@@ -80,8 +139,4 @@ document.getElementById("run-btn")
             packageElement.tableDataElement.classList.remove("table__row--hidden-status-icon");
             packageElement.tableDataElement.classList.add("table__row--visible-status-icon");
         }
-
     });
-
-// document.getElementById("disconnect-usb-adb-btn")
-//     .addEventListener("click", () => connectionManager.closeConnection());
